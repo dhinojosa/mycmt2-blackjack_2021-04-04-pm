2464dd66a92d6c6c3af264d9928f53185c02b878
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2021-04-07 02:05:17 -0600

    GameOutcomeTest and GameOutcome refactored

--- src/main/java/com/jitterted/ebp/blackjack/domain/GameOutcome.java
+++ src/main/java/com/jitterted/ebp/blackjack/domain/GameOutcome.java
@@ -5,8 +5,8 @@ public enum GameOutcome {
     DEALER_BUSTED("Dealer went BUST, Player wins! Yay for you!! 💵"),
     PLAYER_BEATS_DEALER("You beat the Dealer! 💵"),
     PLAYER_PUSHES("Push: Nobody wins, we'll call it even."),
-    PLAYER_LOSES("You lost to the Dealer. 💸");
-    //PLAYER_WINS_BLACKJACK("Player wins Blackjack! 💸");
+    PLAYER_LOSES("You lost to the Dealer. 💸"),
+    PLAYER_WINS_BLACKJACK("Player wins Blackjack! 💸");
 
     private String message;
 
--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -16,11 +16,25 @@ public class GameOutcomeTest {
         //playHits
         //5P. Q
 
-        DeckStub deckStub = new DeckStub(Rank.TWO, Rank.TEN, Rank.TWO, Rank.JACK, Rank.QUEEN);
+        DeckStub deckStub = new DeckStub(Rank.TWO, Rank.TEN, Rank.TWO,
+            Rank.JACK, Rank.QUEEN);
         Game game = new Game(deckStub);
         game.initialDeal(); //2 cards each
         game.playerHits();
 
         assertThat(game.determineOutcome()).isEqualTo(GameOutcome.PLAYER_LOSES);
+    }
+
+    @Test
+    void testBlackJack() {
+        DeckStub deckStub = new DeckStub(
+            Rank.JACK,   //P1
+            Rank.TEN,   //D1
+            Rank.ACE,   //P2
+            Rank.THREE); //D2
+        Game game = new Game(deckStub);
+        game.initialDeal();
+
+        assertThat(game.determineOutcome()).isEqualTo(GameOutcome.PLAYER_WINS_BLACKJACK);
     }
 }
